619a1375f68fa491743c71861157c16f03afd102
Swigut Jan, ITP-IP <Jan.Swigut@itaka.pl>
2019-05-20 16:07:41 +0200

    Initial Commit

---
+++ MasterDetailApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterDetailApp
+//
+//  Created by Swigut Jan, ITP-IP on 20/05/2019.
+//  Copyright © 2019 Swigut Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
